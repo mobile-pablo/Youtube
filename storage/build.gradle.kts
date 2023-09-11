4c57f24001f13a4f49387d8367cce4933e821494
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-09-11 13:05:55 +0200

    Add :storage module

---
+++ storage/build.gradle.kts
@@ -0,0 +1,59 @@
+apply(from = "../ktlint.gradle.kts")
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.kspPlugin)
+}
+
+android {
+    namespace = "com.mobile.pablo.storage"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["room.schemaLocation"] = "${projectDir}/schemas"
+                arguments["room.incremental"] = "true"
+                arguments["room.expandProjection"] = "true"
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.getByPath("preBuild").dependsOn("ktlint")
+
+dependencies {
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    api(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    testImplementation(libs.junit)
+}
