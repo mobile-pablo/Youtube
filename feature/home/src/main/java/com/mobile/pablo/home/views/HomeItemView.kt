7ccf45b00776154535f30e44c5df5396269e4c86
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-11-04 11:02:21 +0100

    Work on Home Item Screen

--- feature/home/src/main/java/com/mobile/pablo/home/views/HomeItemView.kt
+++ feature/home/src/main/java/com/mobile/pablo/home/views/HomeItemView.kt
@@ -1,24 +1,53 @@
 package com.mobile.pablo.home.views
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.tv.material3.Text
 import coil.compose.AsyncImage
 import com.mobile.pablo.home.wrapper.HomeItemWrapper
+import com.mobile.pablo.uicomponents.theme.spacing
+import androidx.compose.material.MaterialTheme as Theme
 
 @Composable
-fun HomeItemView(wrapper: HomeItemWrapper) {
-    Row {
-        Column {
-            AsyncImage(
-                model = wrapper.imageUrl,
-                contentDescription = null
+fun HomeItemView(
+    wrapper: HomeItemWrapper,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(Theme.spacing.spacing_6))
+            .clickable(onClick = ::onHomeItemClick)
+            .padding(
+                vertical = Theme.spacing.spacing_12,
+                horizontal = Theme.spacing.spacing_32
             )
-        }
+    ) {
         Row {
-            Text(text = wrapper.title)
-            Text(text = wrapper.description)
+            Column {
+                AsyncImage(
+                    model = wrapper.imageUrl,
+                    contentDescription = null
+                )
+            }
+            Column {
+                Text(text = wrapper.title)
+                Text(
+                    text = wrapper.description,
+                    maxLines = 3
+                )
+            }
         }
     }
+}
+
+private fun onHomeItemClick() {
 }
