ac8e7024f5ed15fc6bc5eb613c4ff30f3de0993a
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-11-06 23:42:17 +0100

    Clean up Home Screen logic

--- feature/home/src/main/java/com/mobile/pablo/home/screen/HomeScreen.kt
+++ feature/home/src/main/java/com/mobile/pablo/home/screen/HomeScreen.kt
@@ -1,10 +1,17 @@
 package com.mobile.pablo.home.screen
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -16,11 +23,13 @@ import com.mobile.pablo.domain.model.popular.PopularItem
 import com.mobile.pablo.error.screen.destinations.ErrorScreenDestination
 import com.mobile.pablo.home.views.HomeItemView
 import com.mobile.pablo.home.wrapper.HomeItemWrapper
+import com.mobile.pablo.uicomponents.theme.spacing
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.dynamic.within
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import com.ramcosta.composedestinations.utils.navGraph
+import androidx.compose.material.MaterialTheme as Theme
 
 @Composable
 @Destination
@@ -30,33 +39,30 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val popularLazyPagingItems: LazyPagingItems<Popular> = viewModel.popularState.collectAsLazyPagingItems()
-    LazyColumn() {
-        items(popularLazyPagingItems.itemCount) { index ->
-            popularLazyPagingItems[index]!!.items?.forEach {
-                it?.let {
-                    HomeDoneView(it)
+
+    popularLazyPagingItems.apply {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(itemCount) { index ->
+                popularLazyPagingItems[index]!!.items?.forEach {
+                    it?.let {
+                        HomeDoneView(it)
+                    }
                 }
             }
         }
-        popularLazyPagingItems.apply {
-            when {
-                loadState.refresh is LoadState.Loading -> {
-                    item { HomeLoadingView() }
-                }
+        when {
+            loadState.refresh is LoadState.Loading -> HomeLoadingView()
 
-                loadState.refresh is LoadState.Error -> {
-                    val errorState = popularLazyPagingItems.loadState.refresh as LoadState.Error
-                    navigateToErrorScreen(destinationsNavigator, navController, errorState.error)
-                }
+            loadState.refresh is LoadState.Error -> {
+                val errorState = loadState.refresh as LoadState.Error
+                navigateToErrorScreen(destinationsNavigator, navController, errorState.error)
+            }
 
-                loadState.append is LoadState.Loading -> {
-                    item { HomeLoadingView() }
-                }
+            loadState.append is LoadState.Loading -> HomeLoadingView()
 
-                loadState.append is LoadState.Error -> {
-                    val errorState = popularLazyPagingItems.loadState.append as LoadState.Error
-                    navigateToErrorScreen(destinationsNavigator, navController, errorState.error)
-                }
+            loadState.append is LoadState.Error -> {
+                val errorState = loadState.append as LoadState.Error
+                navigateToErrorScreen(destinationsNavigator, navController, errorState.error)
             }
         }
     }
@@ -77,9 +83,18 @@ private fun HomeDoneView(item: PopularItem) {
 @Composable
 private fun HomeLoadingView() {
     Box(
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        LinearProgressIndicator(
+            modifier = Modifier
+                .padding(horizontal = Theme.spacing.spacing_48)
+                .fillMaxWidth()
+                .height(Theme.spacing.spacing_16),
+            color = Color.Red,
+            backgroundColor = Color.LightGray,
+            strokeCap = StrokeCap.Square
+        )
     }
 }
 
