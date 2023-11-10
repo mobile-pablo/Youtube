675537674e3694b36796b809e97b7b158a041d21
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-11-11 00:10:58 +0100

    Update UI (hide Youtube button)

--- feature/player/src/main/java/com/mobile/pablo/player/screen/PlayerScreen.kt
+++ feature/player/src/main/java/com/mobile/pablo/player/screen/PlayerScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.customui.DefaultPlayerUiController
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -30,16 +32,23 @@ fun PlayerScreen(
         verticalArrangement = Arrangement.Center
     ) {
         AndroidView(factory = {
-            val view = YouTubePlayerView(it)
-            view.addYouTubePlayerListener(
-                object : AbstractYouTubePlayerListener() {
-                    override fun onReady(youTubePlayer: YouTubePlayer) {
-                        super.onReady(youTubePlayer)
-                        youTubePlayer.loadVideo(videoId, 0f)
-                    }
+            val youTubePlayerView = YouTubePlayerView(it)
+            youTubePlayerView.enableAutomaticInitialization = false
+            val listener = object : AbstractYouTubePlayerListener() {
+
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    super.onReady(youTubePlayer)
+                    val defaultPlayerUiController = DefaultPlayerUiController(youTubePlayerView, youTubePlayer)
+                    defaultPlayerUiController.showYouTubeButton(false)
+                    youTubePlayerView.setCustomPlayerUi(defaultPlayerUiController.rootView)
+                    youTubePlayer.loadVideo(videoId, 0f)
                 }
-            )
-            view
+            }
+
+            val options: IFramePlayerOptions = IFramePlayerOptions.Builder().controls(0).build()
+
+            youTubePlayerView.initialize(listener, options)
+            youTubePlayerView
         })
     }
 }
