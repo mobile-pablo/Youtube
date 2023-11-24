6982979db1580d5936f88a7c1f26188900b3f6a9
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-11-24 23:02:33 +0100

    Add Custom Controller for Removing opening external app (Copied classes edited)

---
+++ feature/player/src/main/java/com/mobile/pablo/player/player/controller/menu/MenuAdapter.kt
@@ -0,0 +1,43 @@
+package com.mobile.pablo.player.player.controller.menu
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.customui.R
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.customui.menu.MenuItem
+
+/**
+ *  Copied from https://github.com/PierfrancescoSoffritti/android-youtube-player
+ *  only to make YoutubePlayerUiController.kt work (internal class)
+ */
+internal class MenuAdapter(private val context: Context, private val menuItems: List<MenuItem>) :
+    RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.ayp_menu_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.root.setOnClickListener(menuItems[position].onClickListener)
+        holder.textView.text = menuItems[position].text
+        menuItems[position].icon?.let {
+            holder.textView.setCompoundDrawablesWithIntrinsicBounds(
+                ContextCompat.getDrawable(context, it),
+                null, null, null
+            )
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return menuItems.size
+    }
+
+    internal inner class ViewHolder(val root: View) : RecyclerView.ViewHolder(root) {
+        val textView: TextView = root.findViewById(R.id.text)
+    }
+}
--- feature/player/src/main/java/com/mobile/pablo/player/screen/PlayerScreen.kt
+++ feature/player/src/main/java/com/mobile/pablo/player/screen/PlayerScreen.kt
@@ -10,11 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.customui.DefaultPlayerUiController
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import com.mobile.pablo.player.views.youtubePlayerView
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -32,23 +28,7 @@ fun PlayerScreen(
         verticalArrangement = Arrangement.Center
     ) {
         AndroidView(factory = {
-            val youTubePlayerView = YouTubePlayerView(it)
-            youTubePlayerView.enableAutomaticInitialization = false
-            val listener = object : AbstractYouTubePlayerListener() {
-
-                override fun onReady(youTubePlayer: YouTubePlayer) {
-                    super.onReady(youTubePlayer)
-                    val defaultPlayerUiController = DefaultPlayerUiController(youTubePlayerView, youTubePlayer)
-                    defaultPlayerUiController.showYouTubeButton(false)
-                    youTubePlayerView.setCustomPlayerUi(defaultPlayerUiController.rootView)
-                    youTubePlayer.loadVideo(videoId, 0f)
-                }
-            }
-
-            val options: IFramePlayerOptions = IFramePlayerOptions.Builder().controls(0).build()
-
-            youTubePlayerView.initialize(listener, options)
-            youTubePlayerView
+            youtubePlayerView(videoId, it)
         })
     }
 }
