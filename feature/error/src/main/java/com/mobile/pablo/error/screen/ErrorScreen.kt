6af9d9bc14e3f18c336a62b5bed0fb4588f3e823
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-11-24 20:51:05 +0100

    Add Error Screen UI

--- feature/error/src/main/java/com/mobile/pablo/error/screen/ErrorScreen.kt
+++ feature/error/src/main/java/com/mobile/pablo/error/screen/ErrorScreen.kt
@@ -1,29 +1,61 @@
 package com.mobile.pablo.error.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.tv.material3.Text
 import com.mobile.pablo.error.R
 import com.mobile.pablo.uicomponents.theme.bodyTextColor
+import com.mobile.pablo.uicomponents.theme.font
 import com.mobile.pablo.uicomponents.theme.primaryColor
+import com.mobile.pablo.uicomponents.theme.secondaryColor
+import com.mobile.pablo.uicomponents.theme.spacing
 import com.ramcosta.composedestinations.annotation.Destination
 import androidx.compose.material.MaterialTheme as Theme
+
+private const val ERROR_DESCRIPTION_MAX_LINES = 2
 
 @Composable
 @Destination
 fun ErrorScreen() {
-    Box(
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxSize()
             .background(Theme.colors.primaryColor)
+            .padding(horizontal = Theme.spacing.spacing_82)
     ) {
-        Text(
-            text = stringResource(id = R.string.missing_connection_title),
-            color = Theme.colors.bodyTextColor
+        Icon(
+            imageVector = Icons.Filled.Search,
+            modifier = Modifier.size(Theme.spacing.spacing_240),
+            contentDescription = null,
+            tint = Theme.colors.secondaryColor
         )
+        Column(modifier = Modifier.wrapContentSize()) {
+            Text(
+                fontWeight = FontWeight.SemiBold,
+                text = stringResource(id = R.string.missing_connection_title),
+                fontSize = Theme.font.font_28,
+                color = Theme.colors.bodyTextColor
+            )
+            Text(
+                text = stringResource(id = R.string.missing_connection_description),
+                maxLines = ERROR_DESCRIPTION_MAX_LINES,
+                fontSize = Theme.font.font_20,
+                color = Theme.colors.bodyTextColor
+            )
+        }
     }
 }
