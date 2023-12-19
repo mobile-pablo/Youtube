1d5237edfb3111b017ddf0f44fe973a53fa178fc
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-12-19 14:41:09 +0100

    Fix spacing
    Signed-off-by: Paweł Łukasz <pawel.lukasz001@gmail.com>

--- storage/src/main/java/com/mobile/pablo/storage/database/dao/SearchDao.kt
+++ storage/src/main/java/com/mobile/pablo/storage/database/dao/SearchDao.kt
@@ -14,6 +14,7 @@ import com.mobile.pablo.storage.database.entity.search.SearchWithItemEntity
 @Keep
 @Dao
 internal abstract class SearchDao {
+
     @Upsert
     abstract suspend fun upsertSearch(search: SearchEntity)
 
